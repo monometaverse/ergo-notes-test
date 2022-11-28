f79f0fd1e7997acab0543f4d9129efec89bf489c
RegHorace <xiaochi@mono.fun>
2022-11-29 03:13:48 +0800

    修改中

--- contracts/ErgoNotes_simple.sol
+++ contracts/ErgoNotes_simple.sol
@@ -1,16 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-pragma abicoder v2;
 
-import * as symbolName from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
-import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
-import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
 
-/// 永恒之弈抽卡合约
 contract ErgoNotes is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -28,9 +23,5 @@ contract ErgoNotes is ERC721URIStorage, Ownable {
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
-    }
-
-    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
-        s_randomRange = (randomWords[0] % 50) + 1;
     }
 }
